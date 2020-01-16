0fd309f8d54806b70b7a4151909e2fd293e57260
JosephineJefferson <55075046+JosephineJefferson@users.noreply.github.com>
2020-01-15 16:57:45 -0800

    Still very unfinished
    Can now trim data to where the stimulus is high amplitude, and plot that.

--- RayleighSpikePlotter.jl
+++ RayleighSpikePlotter.jl
@@ -14,52 +14,93 @@
 
 using MAT
 using Plots
+using StatsBase
 gr()
 
-
 file = matopen("C:/Users/josep/OneDrive/Desktop/RayleighSpikePlotter/Data/CA87_06_38_02152006_unknown_mgp.mat") #change file here
-spikeTimes = read(file, "spt")
+spikeTimes = read(file, "spt") #spike times in s
 time = read(file, "t") #time in seconds
-dt=time[2]-time[1]
+dt=time[2]-time[1] #timestep, derived from time array
 stimCurve = read(file,"u") #points along curve
+choppedStimCurve= Float64[]
+choppedTime=Float64[]
+choppedSpikeTimes=zeros(length(spikeTimes)) #to put spikes which occur within window of interest
 
-stimulusPlot = plot(time[1:5463], stimCurve[1:5463], legend=false,
-title="Stimulus Waveform")
-display(stimulusPlot)
-
-#Next up: identifying frequencies, and isolating cycles.
-#Can I do calculus on this curve??
-#Probably not... is just a set of points
-
-#Code identifying frequency
-
-possFreq = Float64[0.2, 0.4, 0.8, 1.6, 3.2, 6.4, 12.8]
-
-locOfPeakRAW=findmax(stimCurve)
-cart = (locOfPeak[2])
-startIndex = cart[2] #these 3 lines for extracting index of highest
-count = 0
-i = startIndex
-
-while stimCurve[i]>=stimCurve[i+1] || stimCurve[i-1]>=stimCurve[i+3]
-    global count +=1
-    global i +=1
+#plots stimulus
+function plotStimulusWaveform()
+    stimulusPlot = plot(time[:], stimCurve[:], legend=false,
+    title="Stimulus Waveform")
+    display(stimulusPlot)
 end
 
-cycleTime=count*dt*2
-println(cycleTime)
-approxHz=1/cycleTime
-println(approxHz)
+#plots trimmed stimulus
+function plotChoppedStimulusWaveform()
+    choppedStimulusPlot = plot(choppedTime[:], choppedStimCurve[:],
+    legend=false,title="Trimmed Stimulus Waveform")
+    display(choppedStimulusPlot)
+end
 
-#and then subtract approxHz from each off possFreq
-#square answers. Whichever possFreq produces lowest result
-#must be intended stimulus frequency
+#Code identifying frequency
+function findFrequency()
+    possFreq = Float64[0.2, 0.4, 0.8, 1.6, 3.2, 6.4, 12.8]
+    #all of the frequencies which were tested
+    locOfPeakRAW=findmax(stimCurve)
+    cart = (locOfPeakRAW[2])
+    startIndex = cart[2] #these 3 lines for extracting index of highest
+    count = 0
+    i = startIndex
 
+    while stimCurve[i]>=stimCurve[i+1] || stimCurve[i-1]>=stimCurve[i+3]
+        count +=1
+        i +=1
+    end
 
-#Code chopping out middle bit of stimulus, certain number of
-#high amplitude cycles
+    cycleTime=count*dt*2
+    #println(cycleTime)
+    approxHz=1/cycleTime
+    #println(approxHz)
 
-#Make same chop for all 3 relevant arrays
+    #and then subtract approxHz from each off possFreq
+    #square answers. Whichever possFreq produces lowest result
+    #must be intended stimulus frequency
+end
+
+#trims data to only include region of high stimulus amplitude
+function chopData()
+    positveThresh=percentile(stimCurve[:], 98)
+    negativeThresh=percentile(stimCurve[:], 2)
+    i=1
+    while stimCurve[i]<=positveThresh && stimCurve[i]>=negativeThresh
+        i+=1
+    end
+    j=length(stimCurve[:])
+    while stimCurve[j]<=positveThresh && stimCurve[j]>=negativeThresh
+        j-=1
+    end
+    global choppedStimCurve=stimCurve[i:j]
+    global choppedTime=time[i:j]
+    choppedStart=choppedTime[1]
+    choppedEnd=choppedTime[length(choppedTime)]
+    a=1
+    for k in spikeTimes
+        if k>=choppedStart && k<=choppedEnd
+            global choppedSpikeTimes[a]=k
+            a+=1
+        end
+    end
+    global choppedSpikeTimes=choppedSpikeTimes[1:a-1]
+    # println("start")
+    # println(length(choppedStimCurve))
+    # println(length(choppedTime))
+    # println(choppedStart)
+    # println(choppedEnd)
+    # println(length(choppedSpikeTimes))
+end
+
+plotStimulusWaveform()
+#findFrequency()
+chopData()
+plotChoppedStimulusWaveform()
 
 #Code saying how many cycles we have in chopped bit
 
@@ -67,7 +108,8 @@ println(approxHz)
 
 #function transforming time in s to angle in radians based on frequency
 #OR be more accurate and base transformation on stimCurve at
-#each timepoint
+#each timepoint <- YES do this
+
 
 #for every angle, while angle>2pi, angle=angle-2pi
 
