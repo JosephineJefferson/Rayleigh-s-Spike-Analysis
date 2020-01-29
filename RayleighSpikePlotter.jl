002081dc2d88d7d9ec64af4fdf394220096f470e
JosephineJefferson <55075046+JosephineJefferson@users.noreply.github.com>
2020-01-29 11:59:36 -0800

    Working except shift of fitted function unreliable
    I think it is a problem with arcsin (asin in code).

--- RayleighSpikePlotter.jl
+++ RayleighSpikePlotter.jl
@@ -1,4 +1,4 @@
-#Julia script destined to read Matlab files including
+#Julia script reading Matlab files including
 #spike train data and a sine wave showing the stimulus.
 
 #A part of my LA internship. Will make Rayleigh Z plots
@@ -16,11 +16,9 @@ using MAT
 using Plots
 using StatsBase
 using DSP
-#using LsqFit
-#using ApproxFun
 gr()
 
-file = matopen("C:/Users/josep/OneDrive/Desktop/RayleighSpikePlotter/Data/CA87_15_89_02152006_unknown_mgp.mat") #change file here
+file = matopen("C:/Users/josep/OneDrive/Desktop/RayleighSpikePlotter/Data/CA88_08_33_02222006_3.2_mgp.mat") #change file here
 spikeTimes = read(file, "spt") #spike times in s
 time = read(file, "t") #time in seconds
 dt=time[2]-time[1] #timestep, derived from time array
@@ -35,9 +33,12 @@ A=0
 k=0
 b=0
 #array to store y values of fitted line
-simData=zeros(length(time))
+simStim=zeros(length(time))
 #array to store converted spike times
 sptAsRadians=zeros(length(spikeTimes))
+#values related to mean vector
+meanVsize=0
+meanVangle=0
 
 #plots stimulus
 function plotStimulusWaveform()
@@ -65,7 +66,7 @@ function plotStimRelativeToAngle(chopped=chopping)
         radiansg[co] = timeToRadians(t[co])
         co +=1
     end
-    display(plot(radiansg[:],simData[:]))
+    display(plot(radiansg[:],simStim[:]))
 end
 
 #trims data to only include region of high stimulus amplitude
@@ -92,12 +93,12 @@ function chopData()
         end
     end
     global choppedSpikeTimes=choppedSpikeTimes[1:a-1]
-    global simData = zeros(length(choppedTime))
+    global simStim = zeros(length(choppedTime))
     global sptAsRadians = zeros(length(choppedSpikeTimes))
 end
 
 #finds frequency of high amplitude portion of data
-function mikesPeriodogram(show=false, chopped=chopping)
+function mikesPeriodogram( show=false, correct=true, chopped=chopping)
     if chopped == false
         s = stimCurve[:]
     else
@@ -106,14 +107,28 @@ function mikesPeriodogram(show=false, chopped=chopping)
     pg = periodogram(s[:], fs = 1/dt)         # signal power as a fcn of freq
     i15Hz = minimum(findall(x -> x>15.0, pg.freq)) # index of 15Hz
     f = pg.freq[findmax(pg.power)[2]]     # frequency of peak power
-    if show==true
+    if show
         display(plot(pg.freq[1:i15Hz], pg.power[1:i15Hz],
             title="power", xlabel = "frequency (Hz)")) # plot up to 15Hz
         display(plot(pg.freq[1:i15Hz], pg.power[1:i15Hz], yaxis=:log,
             title="log power", xlabel="frequency (Hz) (nb power at 0 = signal mean )"))
         println("Frequency = ", f, " (", round(f; digits=1), ")")
     end
-    return f
+    freq=f
+    if correct
+        poss = [0.2,0.4,0.8,1.6,3.2,6.4,12.8]
+        smallDiff=20.0
+        count=1
+        while abs(f-poss[count])<smallDiff
+            smallDiff=abs(f-poss[count])
+            freq=poss[count]
+            count+=1
+        end
+        if show
+            println("Corrected Frequency: ", freq)
+        end
+    end
+    return freq
 end
 
 #finds the parameters of the fitted sine wave
@@ -131,7 +146,9 @@ function findParams(chopped=chopping)
     while s[index]>=A || s[index]<=-A
         index+=20
     end
+    #b calculation is unreliable
     global b = asin(s[index]/A)/-k + t[index]
+    #global b = acsc(A/s[index])/-k + t[index]
 end
 
 #called by other functions to make calculations based on
@@ -142,7 +159,7 @@ function yBasedOnTime(t1::Float64)
 end
 
 #plots the fit curve over the stimulus wave
-function plotFitOnStim(chopped=chopping)
+function mapFitOnStim(show=false, chopped=chopping)
     if chopped == false
         t = time[:]
         s = stimCurve[:]
@@ -151,16 +168,18 @@ function plotFitOnStim(chopped=chopping)
         s = choppedStimCurve[:]
     end
     c=1
-    while c<=length(simData)
-        global simData[c]= yBasedOnTime(t[c])
+    while c<=length(simStim)
+        global simStim[c]= yBasedOnTime(t[c])
         c += 1
     end
-    p1=plot(t[:], s[:], label="stimulus")
-    plot!(t[:], simData[:], label="fit")
-    display(p1)
+    if show
+        p1=plot(t[:], s[:], label="stimulus")
+        plot!(t[:], simStim[:], label="fit")
+        display(p1)
+    end
 end
 
-#finds y intercepts of fitted curve and returns an array with
+#finds x intercepts of fitted curve and returns an array with
 #the indexes of these intercepts
 #called by timeToRadians
 function findFittedIntercepts(chopped=chopping)
@@ -169,7 +188,7 @@ function findFittedIntercepts(chopped=chopping)
     else
         t = choppedTime[:]
     end
-    u=simData
+    u=simStim
     index=1
     interceptNo=0
     interceptsInd=zeros(length(t))
@@ -182,13 +201,6 @@ function findFittedIntercepts(chopped=chopping)
         index+=1
     end
     interceptsInd=interceptsInd[1:interceptNo]
-    # interceptvals=zeros(length(interceptsInd))
-    # c=1
-    # while c>=length(interceptsInd)
-    #     interceptvals[c]=u[interceptsInd[c]]
-    #     c+=1
-    # end
-    # println(interceptvals)
     return interceptsInd
 end
 
@@ -201,7 +213,7 @@ function timeToRadians(t1::Float64, chopped=chopping)
     else
         t = choppedTime[:]
     end
-    u=simData[:]
+    u=simStim[:]
     phLen = 1/freq
     intcpt1 = Int(findFittedIntercepts()[1])
     mult=2pi/phLen
@@ -212,11 +224,11 @@ function timeToRadians(t1::Float64, chopped=chopping)
         shift=pi-mult*t[intcpt1]
     end
     answer = t1*mult + shift
-    while answer>=2pi
-        answer=answer-2pi
+    while answer>=2*pi
+        answer=answer-2*pi
     end
     while answer<0
-        answer+=2pi
+        answer+=2*pi
     end
     return answer
 end
@@ -235,14 +247,75 @@ function convertSpikeTimes(chopped=chopping)
     end
 end
 
-#now we do stats!
+#now we do stats! badly, apparently
+function meanVectorCalc(print=false)
+    n = length(sptAsRadians) #-1
+    sumCos=0.0
+    for z in sptAsRadians[:]#[1:length(sptAsRadians)-1]
+        sumCos+=cos(z)
+    end
+    X=sumCos/n
+
+    sumSin=0.0
+    for z in sptAsRadians[:]#[1:length(sptAsRadians)-1]
+        sumSin+=sin(z)
+    end
+    Y=sumSin/n
+
+    r=sqrt((X^2) + (Y^2))
+    global meanVsize=r
+    angle=atan(Y,X)
+    global meanVangle = angle
+    if print
+        println("Vector strength: ", r)
+        if angle<0
+            angle+=2pi
+        end
+        println("Vector angle (rad): ", angle)
+        println("Vector angle (deg): ", rad2deg(angle))
+    end
+end
+
+#function displaying polarplot incl mean vector
+function displayPolar()
+    scale=1
+    if meanVsize<0.01
+        scale=0.02
+    elseif meanVsize<0.05
+        scale=0.1
+    elseif meanVsize<0.1
+        scale=0.2
+    end
+    x=zeros(length(sptAsRadians))
+    count=1
+    while count<=length(x)
+        x[count]=scale+scale/10
+        count+=1
+    end
+
+    sizeArray = 0:meanVsize:meanVsize#*20
+    angleArray = zeros(length(sizeArray))
+    count=1
+    while count <= length(angleArray)
+        angleArray[count]=meanVangle
+        count+=1
+    end
+
+    polarplot=scatter(sptAsRadians[:], x, proj=:polar, label="Spikes")
+    polarplot=plot!(angleArray[:], sizeArray[:], proj=:polar,
+        yaxis=[0,scale+scale/5], linewidth=3, label="Mean Vector")
+    display(polarplot)
+end
 
 #main code, calling functions
 
 if chopping
     chopData()
 end
-freq = mikesPeriodogram()
+freq = mikesPeriodogram(true)
 findParams()
-plotFitOnStim()
-convertSpikeTimes()
+mapFitOnStim(true)
+#convertSpikeTimes()
+#plotStimRelativeToAngle()
+#meanVectorCalc(true)
+#displayPolar()
